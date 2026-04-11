c6421707cc07a88743ae389935b5866007cf3ebf
Pratham <sinhapratham1234@gmail.com>
2026-04-11 18:11:12 +0530

    #fix: changed data types in job model

--- src/db/models/job.model.ts
+++ src/db/models/job.model.ts
@@ -6,31 +6,33 @@ import ExperienceLevel from './experienceLevel.model';
 import JobTitle from './jobTitle.model';
 import sequelize from './sequelize';
 
-class Job extends Model<InferAttributes<Job>, InferCreationAttributes<Job>>{
+class Job extends Model<InferAttributes<Job>, InferCreationAttributes<Job>> {
     declare id: CreationOptional<number>;
     declare title_id: ForeignKey<JobTitle['id']>;
     declare employment_type_id: ForeignKey<EmploymentType['id']>;
     declare experience_level_id: ForeignKey<ExperienceLevel['id']>;
     declare salary_min: number;
     declare salary_max: number;
-    declare recruiter_id: CreationOptional<number>;
+    declare recruiter_id: number;
     declare company_id: ForeignKey<Company['id']>;
-    declare location_id: CreationOptional<number>;
+    declare location_id: number;
     declare apply_link: string;
-    declare description: CreationOptional<string>;
+    declare description: string;
     declare created_at: CreationOptional<Date>;
     declare updated_at: CreationOptional<Date>;
     declare deleted_at: CreationOptional<Date | null>;
 
     declare jobTitle?: NonAttribute<JobTitle>;
+    declare employmentType?: NonAttribute<EmploymentType>;
+    declare experienceLevel?: NonAttribute<ExperienceLevel>;
+    declare company?: NonAttribute<Company>;
 
     static associations: {
-        jobTitle: Association<Job, JobTitle> ;
+        jobTitle: Association<Job, JobTitle>;
         employmentType: Association<Job, EmploymentType>;
-        experienceLevel: Association<Job, ExperienceLevel>
+        experienceLevel: Association<Job, ExperienceLevel>;
         company: Association<Job, Company>;
     };
-
 }
 
 Job.init({
@@ -39,111 +41,96 @@ Job.init({
         autoIncrement: true,
         primaryKey: true,
     },
+
     title_id: {
         type: DataTypes.INTEGER,
-        allowNull: true,
-        references:{
-            model: JobTitle,
-            key: 'id',
-        },
-        onDelete: 'CASCADE'
+        allowNull: false,
+        references: { model: JobTitle, key: 'id' },
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE',
     },
+
     employment_type_id: {
         type: DataTypes.INTEGER,
-        allowNull: true,
-        references:{
-            model: EmploymentType,
-            key: 'id',
-        },
-        onDelete: 'CASCADE',
+        allowNull: false,
+        references: { model: EmploymentType, key: 'id' },
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE',
     },
+
     experience_level_id: {
         type: DataTypes.INTEGER,
-        allowNull: true,
-        references:{
-            model: ExperienceLevel,
-            key: 'id',
-        },
-        onDelete: 'CASCADE'
+        allowNull: false,
+        references: { model: ExperienceLevel, key: 'id' },
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE',
     },
+
     salary_min: {
-        type: DataTypes.DECIMAL(10,2),
+        type: DataTypes.DECIMAL(10, 2),
         allowNull: false,
     },
+
     salary_max: {
-        type: DataTypes.DECIMAL(10,2),
+        type: DataTypes.DECIMAL(10, 2),
         allowNull: false,
     },
+
     recruiter_id: {
         type: DataTypes.INTEGER,
-        allowNull: true,
+        allowNull: false,
     },
+
     company_id: {
         type: DataTypes.INTEGER,
-        allowNull: true,
-        references:{
-            model: Company,
-            key: 'id'
-        },
-        onDelete: 'CASCADE'
+        allowNull: false,
+        references: { model: Company, key: 'id' },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
     },
+
     location_id: {
         type: DataTypes.INTEGER,
-        allowNull: true,
+        allowNull: false,
     },
+
     apply_link: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: {
             name: 'unique_apply_link',
-            msg: 'A job with this application link already exists'
+            msg: 'Job with this apply link already exists',
         },
     },
-    description:{
+
+    description: {
         type: DataTypes.TEXT,
-        allowNull: true,
+        allowNull: false,
     },
+
     created_at: {
         type: DataTypes.DATE,
-        allowNull: true,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
     },
+
     updated_at: {
         type: DataTypes.DATE,
-        allowNull: true,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
     },
+
     deleted_at: {
         type: DataTypes.DATE,
         allowNull: true,
     },
-},{
+},
+{
     tableName: 'jobs',
     sequelize,
     underscored: true,
-    timestamps: false,
-});
-
-// Job.belongsTo(JobTitle, {
-//     foreignKey: 'title_id',
-//     onDelete: 'CASCADE',
-//     as: 'jobTitle'
-// });
-
-// Job.belongsTo(EmploymentType, {
-//     foreignKey: 'employment_type_id',
-//     onDelete: 'CASCADE',
-//     as: 'employmentType'
-// });
-
-// Job.belongsTo(ExperienceLevel, {
-//     foreignKey: 'experience_level_id',
-//     onDelete: 'CASCADE', 
-//     as: 'experienceLevel'
-// });
-
-// Job.belongsTo(Company, {
-//     foreignKey: 'company_id',
-//     onDelete: 'CASCADE', 
-//     as: 'companyId'
-// });
+    timestamps: true,
+}
+);
 
 export default Job;
